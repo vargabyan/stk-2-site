10d61d7475337e211169d5b8cf208d2a5eb9b746
vargabyan <vardan.gabrielyan.it@gmail.com>
2024-05-14 17:21:34 +0500

    #1

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>stk site 2</title>
+</head>
+<body>
+
+<header>
+    <div class="core-container">
+
+    </div>
+</header>
+
+
+<section class="banner">
+    <img class="banner_img" src="/images/heavy-excavator-digging-day-light.png" alt="img">
+    <img class="banner_img-mobile"  src="/images/heavy-excavator-digging-day-light-2.png" alt="img">
+    <div class="core-container banner_content">
+        <h1 class="site-header banner-header">Нерудные материалы <br> Продажа и поставка в Новом Уренгое</h1>
+        <span class="banner_subtitle">Комплексные поставки нерудных материалов: щебня, песка, бетона и железобетонных изделий</span>
+        <button class="banner_btn btn-black">Оптовый заказ</button>
+    </div>
+</section>
+
+
+<footer>
+    <div class="core-container">
+
+    </div>
+</footer>
+
+<script src="scripts.js"></script>
+</body>
+</html>
